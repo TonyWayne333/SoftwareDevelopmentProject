ab8c62bb6be514faed9aa9ce924926c23a7241fe
Neel Shah <neel1040@gmail.com>
2020-01-22 00:57:24 -0500

    Ready to be updated and up to date as older version

--- src/com/development/ProjectController.java
+++ src/com/development/ProjectController.java
@@ -183,7 +183,6 @@ public class ProjectController {
 			    String accessKeyId = "YOUR_ACCESS_KEY";
 			    String secretAccessKey =  "YOUR_SECRET_KEY";
 			    String region = "us-east-2";
-			    //Just for try
 			    String bucketName = "neelbucket4";
 			    
 			    //AWS Access Key ID and Secret Access Key
